3e829ecddff35a5a66f589e00ebfcdfb2b11ea93
realibi <reallibi@gmail.com>
2020-02-17 16:10:03 +0600

    NOD project added

--- src/com/realibi/Runner.java
+++ src/com/realibi/Runner.java
@@ -1,7 +1,26 @@
 package com.realibi;
+import java.lang.*;
+import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Duysenaliev Alibi");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int firstNum = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int secondNum = scanner.nextInt();
+
+        int nod = findNod(firstNum, secondNum);
+        System.out.println("NOD: " + nod);
+    }
+
+    public static int findNod(int firstNum, int secondNum) {
+        if (secondNum == 0){
+            return firstNum;
+        }
+
+        return findNod(secondNum, firstNum % secondNum);
     }
 }
